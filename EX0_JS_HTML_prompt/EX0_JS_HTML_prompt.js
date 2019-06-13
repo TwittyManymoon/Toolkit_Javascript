a6eda7b934e1986bc554dba6d3ae1e83db2acdda
Twitty Manymoon <likimaru-non@hotmail.com>
2019-06-13 15:39:30 +0700

    first commit

---
+++ EX0_JS_HTML_prompt/EX0_JS_HTML_prompt.js
@@ -0,0 +1,8 @@
+/* KEY:
+        prompt = take input from user : prompt(text, default placeholder)
+*/
+
+function WhatsYourName() {
+    myname = prompt("What's your fucking name?", "");
+    alert("Hi, " + myname);
+}
